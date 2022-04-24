fc34faf64a8a2a68e74fc038809f2347f1f00f66
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-04-24 13:12:44 +0600

    Add files via upload

---
+++ components/LandingPage.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Link from 'next/link'
+import { useState } from 'react'
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const LandingPage = () => {
+
+  const [search, setSearch] = useState("")
+  return (
+    <div className="w-full">
+      <div className="flex flex-col text-center w-full mb-20">
+        <div className='bg-gray-900 text-white'>
+          <h1 className="text-3xl text-indigo-500 tracking-widest font-medium title-font mb-16 mt-10">Client Satellite</h1>
+          <div className='flex justify-center'>
+            <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" id="search" name="search" className="bg-white rounded-3xl w-96 border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mb-12" placeholder='Search here any blog'/>
+            <AiOutlineSearch className='text-2xl justify-center mt-2 ml-2'/>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap">
+        <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <a className="text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+        <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">The Catalyzer</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <a className="text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+        <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Neptune</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <a className="text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+        <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+          <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Melanchole</h2>
+          <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+          <a className="text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
