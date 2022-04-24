bad43bf2ca6b8955c53b3ab1ab14914536516872
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-04-24 09:33:36 +0600

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
 
-  const [name, setName] = useState("")
+//   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
 
