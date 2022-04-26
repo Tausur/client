16e125a96b6c1facb570ac2363b33f6caad66353
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-04-26 20:18:47 +0600

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -56,7 +56,7 @@ const Contact = () => {
           <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
           <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
           <div className="relative mb-4">
-   <form action='https://chart.googleapis.com/chart' method='POST'>
+          <form action='https://chart.googleapis.com/chart' method='POST'>
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
             <input method="POST" value={name} onChange={(e)=>setName(e.target.value)} type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
@@ -70,7 +70,7 @@ const Contact = () => {
           </div>
           <button onClick={handleClick} className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
           <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
-</form>
+        </form>
         </div>
       </div>
     </section>
